fd96b2fc2c9b7ababd6376bdef869b82e7d87630
Egor Safronov <heysafronov@gmail.com>
2018-08-26 18:06:28 +0300

    added new component: SidebarItem

---
+++ src/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="sidebar__item" :class="stat" @click="handleClick">
+    <span v-if="errorLog" class="sidebar__item_error">not working</span>
+    <i v-else class="material-icons">{{ icon }}</i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidebarItem",
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    stat: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      errorLog: false
+    };
+  },
+  methods: {
+    handleClick() {
+      if (!this.stat) {
+        this.errorLog = !this.errorLog;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar__item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 25px;
+  color: #c0c5d2;
+  width: 82px;
+  height: 85px;
+  cursor: pointer;
+  &_error {
+    font-size: 9px;
+    color: white;
+    background-color: #f0166d;
+    opacity: 0.5;
+    padding: 2px 5px;
+    border-radius: 2px;
+  }
+  &:hover {
+    color: #0077ff;
+  }
+}
+.active {
+  border-right: 3px solid #0077ff;
+  color: #0077ff;
+}
+</style>
--- src/components/Sidebar/TheSidebar.vue
+++ src/components/Sidebar/TheSidebar.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="sidebar">
     <div class="sidebar__bs"></div>
-    <div class="sidebar__item dashboard">
-      <i class="material-icons">dashboard</i>
-    </div>
-    <div class="sidebar__item forum">
-      <i class="material-icons">forum</i>
-    </div>
-    <div class="sidebar__item">
-      <i class="material-icons">cloud</i>
-    </div>
-    <div class="sidebar__item">
-      <i class="material-icons">email</i>
-    </div>
-    <div class="sidebar__item">
-      <i class="material-icons">settings</i>
-    </div>
+    <SidebarItem :icon="icons.dashboard" :stat="status"/>
+    <SidebarItem :icon="icons.forum"/>
+    <SidebarItem :icon="icons.cloud"/>
+    <SidebarItem :icon="icons.email"/>
+    <SidebarItem :icon="icons.settings"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TheSidebar"
+  name: "TheSidebar",
+  components: {
+    SidebarItem: () => import("./SidebarItem.vue")
+  },
+  data() {
+    return {
+      icons: {
+        settings: "settings",
+        email: "email",
+        cloud: "cloud",
+        forum: "forum",
+        dashboard: "dashboard"
+      },
+      status: "active"
+    };
+  }
 };
 </script>
 
@@ -33,27 +38,8 @@ export default {
   background-color: white;
   width: 85px;
   box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.3);
-  &__item {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    font-size: 25px;
-    color: #c0c5d2;
-    width: 82px;
-    height: 85px;
-    cursor: pointer;
-    &:hover {
-      color: #0077ff;
-    }
-  }
   &__bs {
     height: 50px;
   }
-}
-
-.dashboard {
-  border-right: 3px solid #0077ff;
-  color: #0077ff;
 }
 </style>
