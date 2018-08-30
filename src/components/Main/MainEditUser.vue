32b35ffaf0a69d54c2a7edd08c0943215a3cb5e9
Egor Safronov <heysafronov@gmail.com>
2018-08-30 18:01:51 +0300

    edited MainEditUser

--- src/components/Main/MainEditUser.vue
+++ src/components/Main/MainEditUser.vue
@@ -1,30 +1,35 @@
 <template>
-  <div>
-    <div class="edit-user">
-      Editing user: <span>{{ nameUser }}</span>
+  <div class="form">
+    <div class="form__title">
+      Editing user:<span>{{ nameUser }}</span>
     </div>
-    <div class="edit-form">
-      <div>
-        <label>ID</label>
-        <input type="text" v-model="nameUser"/>
+    <div class="form__controls-wrapper">
+      <div class="form__controls">
+        <div class="controls-stack">
+          <div class="wrapper-control">
+            <input class="form-control" type="text" v-model="nameUser"/>
+          </div>
+          <div class="wrapper-control">
+            <input class="form-control" type="text" v-model="nameUser"/>
+          </div>
+        </div>
+      <div class="controls-stack">
+        <div class="wrapper-control">
+          <input class="form-control" type="text" v-model="nameUser"/>
+        </div>
+        <div class="wrapper-control">
+          <input class="form-control" type="text" v-model="nameUser"/>
+        </div>
       </div>
-      <div>
-        <label>Email</label>
-        <input type="text" v-model="nameUser"/>
+      <div class="controls-stack">
+        <div class="wrapper-control">
+          <input class="form-control" type="text" v-model="nameUser"/>
+        </div>
+        <div class="wrapper-control">
+          <div :class="{saved, editing}">SAVED</div>
+        </div>
       </div>
-      <div>
-        <label>REGISTRATION</label>
-        <input type="text" v-model="nameUser"/>
       </div>
-      <div>
-        <label>DEVELOPER</label>
-        <input type="text" v-model="nameUser"/>
-      </div>
-      <div>
-        <label>COUNTRY</label>
-        <input type="text" v-model="nameUser"/>
-      </div>
-      <div v-show="saveStatus">SAVED</div>
     </div>
   </div>
 </template>
@@ -34,7 +39,8 @@ export default {
   name: "MainAddUser",
   data() {
     return {
-      saveStatus: false
+      saved: false,
+      editing: true
     };
   },
   computed: {
@@ -46,7 +52,8 @@ export default {
         return this.$store.getters.USERS[this.id].name;
       },
       set(value) {
-        this.saveStatus = true;
+        this.saved = true;
+        this.editing = false;
         this.$store.commit("UPDATE_NAME", { value: value, id: this.id });
         setTimeout(this.changeSaveStatus, 300);
       }
@@ -54,28 +61,82 @@ export default {
   },
   methods: {
     changeSaveStatus() {
-      this.saveStatus = false;
+      this.saved = false;
+      this.editing = true;
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.edit-user {
-  display: flex;
-  align-items: center;
-  width: 1402px;
-  height: 90px;
-  margin: 0 auto;
-  font-size: 21px;
-  letter-spacing: 0.14px;
-  color: #1a173b;
-  span {
-    color: #8a96a0;
+.form {
+  &__title {
+    display: flex;
+    align-items: center;
+    width: 1402px;
+    height: 90px;
+    margin: 0 auto;
+    font-size: 21px;
+    letter-spacing: 0.14px;
+    color: #1a173b;
+    span {
+      color: #0077ff;
+      margin-left: 10px;
+    }
   }
-}
-  .edit-form {
+  &__controls-wrapper {
     width: 1402px;
     margin: 0 auto;
+    border: 1px solid #ebedf8;
+    border-radius: 4px;
+    .form__controls {
+      padding: 50px;
+      .form-control {
+        border: 1px solid #ebedf8;
+        border-radius: 4px;
+        color: #8a96a0;
+        padding: 10px;
+        outline: none;
+        font-size: 15px;
+        letter-spacing: 0.5px;
+      }
+      .controls-stack {
+        display: flex;
+        .wrapper-control {
+          padding: 10px;
+          .editing {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 214px;
+            height: 39px;
+            border-radius: 4px;
+            background: repeating-linear-gradient(
+              45deg,
+              white,
+              white 10px,
+              #0db07a 10px,
+              #0db07a 20px
+            );
+            color: white;
+            font-size: 15px;
+            letter-spacing: 0.5px;
+          }
+          .saved {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 214px;
+            height: 39px;
+            border-radius: 4px;
+            background-color: #0db07a;
+            color: white;
+            font-size: 15px;
+            letter-spacing: 0.5px;
+          }
+        }
+      }
+    }
   }
+}
 </style>
