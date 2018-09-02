041a532d290036b87e2a8196ef8d98a40546ad03
Egor Safronov <heysafronov@gmail.com>
2018-09-02 17:46:01 +0300

    edited AddUser / set id

--- src/components/Main/Add/AddUser.vue
+++ src/components/Main/Add/AddUser.vue
@@ -100,7 +100,7 @@ export default {
       }
     },
     setId() {
-      this.newUser.id = this.$store.getters.USERS.length + 100;
+      this.newUser.id = this.$store.getters.USERS.length;
     },
     setDate() {
       this.newUser.reg = new Date().toLocaleDateString();
