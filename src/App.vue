12e4b6915a59fdb868dbed9aa4250751b3a966f4
Egor Safronov <heysafronov@gmail.com>
2018-09-02 12:26:49 +0300

    edited modal box component

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,13 @@
 <template>
   <div id="app">
-    <transition appear name="modal">
-      <div v-if="modal" class="modal" key="modal">
-        <div class="modal__text">This app is for full-hd resolution 😎</div>
-        <button type="button" class="modal__btn" @click="showModal">Open</button>
-      </div>
-      <div v-else key="app">
-        <TheHeader/>
-        <div class="main-wrapper">
-          <TheSidebar/>
-          <TheMain/>
-        </div>
-      </div>
-    </transition>
+    <ModalBox/>
   </div>
 </template>
 
 <script>
 export default {
   components: {
-    TheHeader: () => import("./components/Header/HeaderApp.vue"),
-    TheSidebar: () => import("./components/Sidebar/SidebarApp.vue"),
-    TheMain: () => import("./components/Main/MainApp.vue")
-  },
-  data() {
-    return {
-      modal: true
-    };
-  },
-  methods: {
-    showModal() {
-      this.modal = false;
-    }
+    ModalBox: () => import("./components/ModalBox.vue")
   }
 };
 </script>
-
-<style lang="scss">
-@import "assets/reset.css";
-
-#app {
-  margin: 50px auto 10px auto;
-  width: 1580px;
-}
-.modal {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  width: 300px;
-  background-color: white;
-  border-radius: 4px;
-  margin: 0 auto;
-  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.3);
-  &__btn {
-    padding: 8px 15px;
-    border-radius: 4px;
-    background-color: #0077ff;
-    color: white;
-    font-size: 14px;
-    border: none;
-    cursor: pointer;
-    outline: none;
-    letter-spacing: 0.5px;
-    margin: 0 0 20px 0;
-    &:hover {
-      box-shadow: 0px 0px 10px 0px #9ea7fc;
-    }
-  }
-  &__text {
-    color: #8a96a0;
-    margin: 20px 0;
-  }
-  &-enter-active {
-    transition: all 0.3s ease;
-  }
-  &-leave-active {
-    transition: all 0.01s cubic-bezier(1, 0.5, 0.8, 1);
-  }
-  &-enter {
-    transform: translateX(10px);
-    opacity: 0;
-  }
-  &-leave-to {
-    transform: translateX(10px);
-    opacity: 0;
-  }
-}
-.main-wrapper {
-  display: flex;
-}
-</style>
---
+++ src/components/ModalBox.vue
@@ -0,0 +1,90 @@
+<template>
+  <transition appear name="modal">
+    <div v-if="modal" class="modal" key="modal">
+      <div class="modal__text">This app is for full-hd resolution 😎</div>
+      <button class="modal__btn" type="button" @click="showModal">Open</button>
+    </div>
+    <div v-else key="app">
+      <HeaderApp/>
+      <div class="main-wrapper">
+        <SidebarApp/>
+        <MainApp/>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  components: {
+    HeaderApp: () => import("../components/Header/HeaderApp.vue"),
+    SidebarApp: () => import("../components/Sidebar/SidebarApp.vue"),
+    MainApp: () => import("../components/Main/MainApp.vue")
+  },
+
+  data() {
+    return {
+      modal: true
+    };
+  },
+
+  methods: {
+    showModal() {
+      this.modal = false;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../assets/reset.css";
+
+.modal {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  background-color: white;
+  border-radius: 4px;
+  margin: 0 auto;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.3);
+  &__btn {
+    padding: 8px 15px;
+    border-radius: 4px;
+    background-color: #0077ff;
+    color: white;
+    font-size: 14px;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    letter-spacing: 0.5px;
+    margin: 0 0 20px 0;
+    &:hover {
+      box-shadow: 0px 0px 10px 0px #9ea7fc;
+    }
+  }
+  &__text {
+    color: #8a96a0;
+    margin: 20px 0;
+  }
+  &-enter-active {
+    transition: all 0.3s ease;
+  }
+  &-leave-active {
+    transition: all 0.01s cubic-bezier(1, 0.5, 0.8, 1);
+  }
+  &-enter {
+    transform: translateX(10px);
+    opacity: 0;
+  }
+  &-leave-to {
+    transform: translateX(10px);
+    opacity: 0;
+  }
+}
+
+.main-wrapper {
+  display: flex;
+}
+</style>
